dbee6beb9d3c70f3d53162efa7500ebda1aa9d75
Cory Selvidge <healincheese@gmail.com>
2019-06-28 14:20:29 -0700

    Fixed sections controller error

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -14,7 +14,7 @@ class Instructor::SectionsController < ApplicationController
   private
 
   def require_authorized_for_current_course
-    if current_course_user != current_user
+    if current_course.user != current_user
       render plain: "Unauthorized", status: :unauthorized
     end
   end
