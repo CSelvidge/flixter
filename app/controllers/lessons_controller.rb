e30a3fb273f8868f764e6964f5ebc0e8ceb504d6
Cory Selvidge <healincheese@gmail.com>
2019-07-10 16:11:17 -0700

    Payments with Stripe

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,18 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_lesson, only: [:show]
+
+
   def show
   end
 
   private
+
+  def require_authorized_for_current_lesson
+    if current_lesson.section.course.user.enrolled_in?(@course) == false
+      redirect_to courses_path(@course), alert: 'Unauthorized for content.'
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
